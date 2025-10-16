353374292b3cc8dc4ba3707d3258b1577c22a7dd
JorgemMrtnz <jorgefdo.martinezfdez@gmail.com>
2025-10-16 11:56:01 +0200

    AkAkAkA

---
+++ ladllwadlwal.py
@@ -0,0 +1,27 @@
+#CARA O HOLA
+import random
+numero = random.randint(1, 10)
+if numero == 1:
+    print("1")
+if numero == 2:
+    print("2")
+if numero == 3:
+    print("3")
+if numero == 4:
+    print("4")
+if numero == 5:
+    print("5")
+if numero == 6:
+    print("6")
+if numero == 7:
+    print("7")
+if numero == 8:
+    print("8")
+if numero == 9:
+    print("9")
+if numero == 10:
+    print("10")
+if numero % 2 == 0:
+    print("simon")
+else:
+    print("limonada")             
